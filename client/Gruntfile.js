524189cd152597062c875fc0dbf2a43551ff5cc5
asinha <asinha@zaloni.com>
2018-08-08 01:15:21 +0530

    copy html in build (fix field template issue)

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -421,7 +421,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+          'styles/fonts/{,*/}*.*',
+          'views/**'
           ]
         }, {
           expand: true,
