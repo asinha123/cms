f7cd4faf43fa9f5903170db25549e476ca21d077
asinha <asinha@zaloni.com>
2018-08-09 11:21:20 +0530

    console error fix

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,19 +9,22 @@
   function init() {
     $scope.profile = userService.getLoggedInUser();
     var options = {};
-    options.userId = $scope.profile.userId;
-    contactService.getContactList(options)
-      .then(function (response) {
-        var data = [];
-        if (response.data.result) {
-          data = response.data.result.contactList;
-          _.each(data, function (contact) {
-            contact.groupName = contact.group ? contact.group.groupName : null;
-          });
-        }
-      }, function (err) {
-        messagePopupService.openGrowlError(err.statusText);
-      });
+    if ($scope.profile) {
+      options.userId = $scope.profile.userId;
+      contactService.getContactList(options)
+        .then(function (response) {
+          var data = [];
+          if (response.data.result) {
+            data = response.data.result.contactList;
+            _.each(data, function (contact) {
+              contact.groupName = contact.group ? contact.group.groupName : null;
+            });
+          }
+        }, function (err) {
+          messagePopupService.openGrowlError(err.statusText);
+        });
+    }
+
 
   }
 
